eb56279c81b2aafee57e4612a5d3eb4b6810afb8
Wombeez <jdpeck90@gmail.com>
2018-04-12 14:15:21 -0400

    pushing more improvements

--- portfolio/src/Components/Education/EducationContent.js
+++ portfolio/src/Components/Education/EducationContent.js
@@ -33,24 +33,26 @@ class EducationCOntent extends Component {
     
     render() { 
         return (
-            <div className="education_page container">
+            <div className="education container">
                 <div className="education_title">
                     <h1>EDUCATION</h1>
                 </div>
-                <div className="education_body">
-                    <ul className='education_list'>
-                        <li onClick={this.onClick.bind(this,0)} className="ed_item ed_ga hoverDivLG"><a href="#">General Assembly</a></li>
-                        <li onClick={this.onClick.bind(this,1)} className="ed_item ed_aa hoverDivLG"><a href="#">App Academy</a></li>
-                        <li onClick={this.onClick.bind(this,2)} className="ed_item ed_uo hoverDivLG"><a href="#">University of Oregon</a></li>
-                       
-                    </ul>
+                <div className="education_wrapper">
+                    <div className="education_body">
+                        <ul className='education_list'>
+                            <li onClick={this.onClick.bind(this,0)} className="ed_item ed_ga hoverDivLG"><a href="#">General Assembly</a></li>
+                            <li onClick={this.onClick.bind(this,1)} className="ed_item ed_aa hoverDivLG"><a href="#">App Academy</a></li>
+                            <li onClick={this.onClick.bind(this,2)} className="ed_item ed_uo hoverDivLG"><a href="#">University of Oregon</a></li>
+                        
+                        </ul>
+                    </div>
+                    <div className="education_body">
+                      
+                            {this.state.showItems[0] ? <CourseGA /> : null} 
+                             {this.state.showItems[1] ? <CourseAA /> : null} 
+                           {this.state.showItems[2] ? <CourseUO /> : null} 
+                        
                 </div>
-                <div className="education_body">
-                    <ul className='education_list'>
-                        <li> {this.state.showItems[0] ? <CourseGA /> : null} </li>
-                        <li>  {this.state.showItems[1] ? <CourseAA /> : null} </li>
-                        <li> {this.state.showItems[2] ? <CourseUO /> : null} </li>
-                    </ul>
               </div>
             </div>
           )
